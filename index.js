2352ac27a4510f7a19726be7f0df3c2e97133672
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-15 14:34:55 +0300

    write play 5 round game

--- index.js
+++ index.js
@@ -22,6 +22,42 @@ function getUserChoice() {
 }
 
 function playRound(computerChoice,userChoice) {
-
+    if(userChoice === computerChoice) {
+        userScore++;
+        computerScore++;
+        console.log("Draw");
+    } else if(userChoice === "rock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "paper" || userChoice === "paper" && computerChoice === "rock") {
+        userScore++;
+        console.log("User Won");
+    } else {
+        computerScore++;
+        console.log("Computer Won");
+    }
 }
 
+
+
+function playGame() {
+    
+    for (let round = 1; round <= 5; round++) {
+        const computerChoice = getComputerChoice();
+        const userChoice = getUserChoice();
+        
+        playRound(computerChoice,userChoice);
+    }
+    
+    let result;
+    if(userScore === computerScore) {
+        result = "Draw";
+    } else if ( userScore > computerScore) {
+        result = "user won";
+    } else {
+        result = "computer won";
+    }
+
+    console.log(`Result:${result}\n
+        Computer Score: ${computerScore}\n
+        User Score: ${userScore}`);
+}
+
+playGame();
