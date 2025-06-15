bbbdc98d17b881d5e7f28ee673c84583b3fa856a
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-15 23:48:23 +0300

    fix event listener bug when you click container
    i add event listener to container which click on spaces in the container
    still listen to it which casue problem so i added this fix

--- index.js
+++ index.js
@@ -64,33 +64,6 @@ function resetTheGame() {
     gameFinished = false;
 }
 
-
-
-// function playGame() {
-    
-//     for (let round = 1; round <= 5; round++) {
-//         const computerChoice = getComputerChoice();
-//         const userChoice = getUserChoice();
-        
-//         playRound(computerChoice,userChoice);
-//     }
-    
-//     let result;
-//     if(userScore === computerScore) {
-//         result = "Draw";
-//     } else if ( userScore > computerScore) {
-//         result = "user won";
-//     } else {
-//         result = "computer won";
-//     }
-
-//     console.log(`Result:${result}\n
-//         Computer Score: ${computerScore}\n
-//         User Score: ${userScore}`);
-// }
-
-// playGame();
-
 const btns = document.querySelector('#buttons-container');
 
 btns.addEventListener("click",(e) => {
